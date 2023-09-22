d26deddbbd9fc4e465cc80b126bb73bee61c38fc
Antoine Aube <aaube3@gmail.com>
2023-09-22 13:30:45 -0600

    feature: add new discount form

--- app/controllers/merchants/bulk_discounts_controller.rb
+++ app/controllers/merchants/bulk_discounts_controller.rb
@@ -7,4 +7,25 @@ class Merchants::BulkDiscountsController < ApplicationController
   def show 
     @discount = BulkDiscount.find(params[:discount_id])
   end
+
+  def new
+    @merchant = Merchant.find(params[:merchant_id])
+  end
+    
+  def create
+    merchant = Merchant.find(params[:merchant_id])
+    discount = merchant.bulk_discounts.new(discount_params)
+    discount.save
+    if discount.save
+      redirect_to merchant_bulk_discounts_path(merchant)
+    elsif !discount.save
+      redirect_to new_merchant_bulk_discount_path(merchant)
+      flash[:error] = "Discount not created: Required information missing."
+    end
+  end
+
+  private
+  def discount_params
+    params.permit(:name, :percentage, :item_threshold)
+  end
 end
