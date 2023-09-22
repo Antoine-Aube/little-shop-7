aec2508bf81fdc3e0f8d219f851fe364722b1f83
Antoine Aube <aaube3@gmail.com>
2023-09-22 11:43:32 -0600

    feature: bulk discounts index page with controller action

--- app/controllers/merchants/bulk_discounts_controller.rb
+++ app/controllers/merchants/bulk_discounts_controller.rb
@@ -1,3 +1,6 @@
-class BulkDiscountsController < ApplicationController
-  
+class Merchants::BulkDiscountsController < ApplicationController
+  def index
+    @merchant = Merchant.find(params[:merchant_id])
+    @discounts = @merchant.bulk_discounts
+  end
 end
