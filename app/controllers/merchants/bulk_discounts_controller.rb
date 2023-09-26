8387443a192482da7b33ba883d8c1839fce5fe46
Antoine Aube <aaube3@gmail.com>
2023-09-25 19:21:08 -0600

    API extension working for displaying next three holidays

--- app/controllers/merchants/bulk_discounts_controller.rb
+++ app/controllers/merchants/bulk_discounts_controller.rb
@@ -2,6 +2,7 @@ class Merchants::BulkDiscountsController < ApplicationController
   def index
     @merchant = Merchant.find(params[:merchant_id])
     @discounts = @merchant.bulk_discounts
+    @upcoming_holidays = HolidaysService.next_three_holidays
   end
 
   def show
